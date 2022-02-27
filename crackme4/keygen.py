2d3f18a4d54f031d93ae39edef471daa500f556b
ThePhilosopherV <42734412+ThePhilosopherV@users.noreply.github.com>
2022-02-27 12:13:49 +0100

    Add files via upload

--- crackme4/keygen.py
+++ crackme4/keygen.py
@@ -1,66 +1,26 @@
 import random
 import string
 
-
-chars = string.ascii_letters.lower() #
-
+print "It might take a few seconds to produce a key"
+chars = string.ascii_letters.lower() # lower case values gave a quick result
 
 
 while (1):
     key1=''.join(random.choice(chars) for x in range(12))
+    #lets find a key that will pass the first phase of the key checking algorithm
+    #we will test only the characters used in the first phase namely characters at index :6,7,9,10,11
     val = (ord(key1[11])+ord(key1[6]))*ord(key1[9])+ord(key1[10])*ord(key1[7])
     if val == 0x9af2:
         while(1):
             key=''.join(random.choice(chars) for x in range(12))
-            #key=key1
-            esi = ord(key[0])
-            xmm0 = ord(key[0])
-            edi = ord(key[1])
-            eax = ord(key[2])
-            xmm5 = ord(key[2])
-            eax = ord(key[3])
-            
-            eax = ord(key[3])-0
-            eax = (ord(key[3])>>1)
-
-            xmm5 = (ord(key[2])*0.5) 
-            xmm4 = eax=(ord(key[3])>>1)
-            eax = ord(key[4])
-            edx = ord(key[4])
-            edx = (ord(key[4])>>1)
-            xmm3 = (ord(key[4])>>1)
-            edx = (ord(key[4])>>1)*(ord(key[4])>>1)
-            ecx = ord(key[5])
-
-            eax = ord(key[5])
-            eax = ord(key[5]) * ord(key[1])
-            xmm1 = ord(key[5])
-
-            eax = (ord(key[5]) * ord(key[1]))-((ord(key[4])>>1)*(ord(key[4])>>1))
-
-
-            xmm2 = (ord(key[5]) * ord(key[1]))-((ord(key[4])>>1)*(ord(key[4])>>1))
-            
-
-            xmm1 =  xmm1*xmm5
-            xmm2 = xmm2 * xmm0
-            xmm0 = xmm3
-            xmm0 = xmm0*xmm4
-            xmm3 = xmm3*xmm5
-            xmm1 = xmm1 - xmm0
-            xmm0 = edi
-            xmm1 = xmm1*xmm5
-            xmm0 = xmm0*xmm4
-            xmm2 = xmm2 - xmm1
-            xmm3 = xmm3 - xmm0
-            xmm3 = xmm3 * xmm4
-            xmm2 = xmm2 + xmm3
-
-            if xmm2 == 733898.75 :
+           
+            #here we passed the first phase and we will need to find the combination of
+            #characters that will pass the second phase we will process only characters 
+            #used in second phase namely characters at index : 0,1,2,3,4,5
+            if  (( (((ord(key[5]) * ord(key[1])) - ((ord(key[4]) >> 1) * (ord(key[4]) >> 1))) * ord(key[0]) ) - ((( ord(key[5]) * (ord(key[2]) * 0.5 ) ) - ( (ord(key[4]) >> 1) * (ord(key[3]) >> 1) )) * (ord(key[2]) * 0.5 )) ) + (( ( (ord(key[4]) >> 1) * (ord(key[2]) * 0.5 ) ) - ( ord(key[1]) * (ord(key[3]) >> 1) ) ) * (ord(key[3]) >> 1) )) == 733898.75 :
                 
-                print key[0:6]+key1[6]+key1[7]+"x"+key1[9]+key1[10]+key1[11]
+                #after the second condition is met we combine both characters of first and second phase 
+                print key[0:6]+key1[6:12]
                 
                 quit()
-                
             
-    
